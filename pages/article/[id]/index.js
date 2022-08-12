914a9db2c44268b79acfd0fa42a97ef90e2256cb
Jennifer Lang <jenniferlang1921@gmail.com>
2022-08-11 22:37:57 -0500

    fix class error

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -10,7 +10,7 @@ const article = ({ article }) => {
   return (
     <>
       <Meta title={article.title} description={article.excerpt} />
-      <div class="container  mx-auto">
+      <div className="container mx-auto">
         <h1 className="text-center pt-60 mx-12 text-3xl ">{article.title}</h1>
         <div className="border-b-4  border-red-900 w-1/3 mx-auto p-2 mb-8"></div>
         <p>{article.body}</p>
