586cb0df13d2643a346871a806cecaaf7b99b08e
Jennifer Lang <jenniferlang1921@gmail.com>
2022-09-02 14:58:32 -0500

    google adsense

--- pages/_document.js
+++ pages/_document.js
@@ -22,6 +22,12 @@ export default class MyDocument extends Document {
           `,
             }}
           />
+
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9500426396658400"
+            crossorigin="anonymous"
+          ></script>
         </Head>
         <body>
           <Main />
